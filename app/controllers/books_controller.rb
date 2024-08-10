0aa741f6984ea734d08c7ba338404f462891ec6b
RikuTakayama <ec2-user@ip-172-31-24-155.ap-northeast-1.compute.internal>
2024-08-10 03:15:11 +0000

    Break 8/10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,10 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(list_params)
+    book = Book.new(book_params)
     book.save
-    redirect_to '/books'
+     @book = book 
+    render :show
   end
 
   def show
@@ -22,10 +23,18 @@ class BooksController < ApplicationController
   def update
     book=Book.find(params[:id])
     book.update(book_params)
+    
     redirect_to book_path(book.id)
   end
-  private
-  def list_params
+
+def destroy
+  book=Book.find(params[:id])
+  book.destroy
+  redirect_to'/books'
+  
+end
+private
+  def book_params
     params.require(:book).permit(:title, :body)
   end
 end
