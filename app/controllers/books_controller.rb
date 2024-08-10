3317cadeace145e954bdbc95f9c3894f0ad39e6b
RikuTakayama <ec2-user@ip-172-31-24-155.ap-northeast-1.compute.internal>
2024-08-10 06:50:56 +0000

    バリデーション設定finish 8/10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,10 +5,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-     @book = book 
-    render :show
+   @book = Book.new(book_params)
+   if @book.save
+     redirect_to book_path(@book.id)
+   else
+     @books=Book.all
+    render :index
+   end
   end
 
   def show
@@ -21,17 +24,20 @@ class BooksController < ApplicationController
   end
 
   def update
-    book=Book.find(params[:id])
-    book.update(book_params)
-    
-    redirect_to book_path(book.id)
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id)
+    else
+     
+    render :edit
+    end
   end
 
 def destroy
   book=Book.find(params[:id])
   book.destroy
   redirect_to'/books'
-  
+
 end
 private
   def book_params
