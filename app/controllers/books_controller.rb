d11f8a8f73d7243a0ba6b53d6f4dd21b1e6bcc7a
RikuTakayama <ec2-user@ip-172-31-24-155.ap-northeast-1.compute.internal>
2024-08-11 03:11:33 +0000

    FInish 8/11

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,9 +7,10 @@ class BooksController < ApplicationController
   def create
    @book = Book.new(book_params)
    if @book.save
-     
+     flash[:notice]="Book was successfully created."
      redirect_to book_path(@book.id)
    else
+
      @books=Book.all
     render :index
    end
@@ -27,9 +28,10 @@ class BooksController < ApplicationController
   def update
     @book=Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice]="Book was successfully updated"
     redirect_to book_path(@book.id)
     else
-     
+
     render :edit
     end
   end
